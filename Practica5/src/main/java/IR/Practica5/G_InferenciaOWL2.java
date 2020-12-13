baae4d28543ce7eedd610d7909277c28d23a6362
abeln94 <544125@unizar.es>
2020-12-13 12:25:01 +0100

    ejercicio 6 segunda parte

--- Practica5/src/main/java/IR/Practica5/G_InferenciaOWL2.java
+++ Practica5/src/main/java/IR/Practica5/G_InferenciaOWL2.java
@@ -1,16 +1,10 @@
 package IR.Practica5;
 
-import org.apache.jena.rdf.model.InfModel;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
+import openllet.jena.PelletReasonerFactory;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
-
-import openllet.jena.PelletReasonerFactory;
 
 /**
  * Crea un modelo con una jerarquia de clases y propiedades y le aplica el modelo
@@ -25,27 +19,27 @@ public class G_InferenciaOWL2 {
 	
 		// contruimos un modelo secillo de seres vivos y sus relaciones
 		Model rdfsExample = ModelFactory.createDefaultModel();
-		//Definimos una jerarquía de clases de seres vivos
-		Resource animalClass = rdfsExample.createResource(NS+"Animal");
-		animalClass.addProperty(RDF.type, RDFS.Class);
-		Resource mamiferoClass = rdfsExample.createResource(NS+"Mamifero");
-		animalClass.addProperty(RDFS.subClassOf, mamiferoClass);
-		Resource humanoClass = rdfsExample.createResource(NS+"Humano");
-		humanoClass.addProperty(RDFS.subClassOf, animalClass);
-		
-		//definimos las relaciones hijo, e hijo adoptivo
-		Property hijoDe = rdfsExample.createProperty(NS, "hijoDe");
-		//Property padreDe = rdfsExample.createProperty(NS, "padreDe");
-		//padreDe.addProperty(OWL.inverseOf, hijoDe);
-		
-		rdfsExample.add(hijoDe, RDF.type, RDF.Property);
-		hijoDe.addProperty(RDFS.domain, animalClass);
-		hijoDe.addProperty(RDFS.range, animalClass);
-		
-		Property hijoAdoptDe = rdfsExample.createProperty(NS, "hijoAdoptivoDe");
-		rdfsExample.add(hijoAdoptDe, RDFS.subPropertyOf, hijoDe);
-		hijoAdoptDe.addProperty(RDFS.domain, humanoClass);
-		hijoAdoptDe.addProperty(RDFS.range, humanoClass);
+        //Definimos una jerarquía de clases de seres vivos
+        Resource animalClass = rdfsExample.createResource(NS + "Animal");
+        animalClass.addProperty(RDF.type, RDFS.Class);
+        Resource mamiferoClass = rdfsExample.createResource(NS + "Mamifero");
+        animalClass.addProperty(RDFS.subClassOf, mamiferoClass);
+        Resource humanoClass = rdfsExample.createResource(NS + "Humano");
+        humanoClass.addProperty(RDFS.subClassOf, animalClass);
+
+        //definimos las relaciones hijo, e hijo adoptivo
+        Property hijoDe = rdfsExample.createProperty(NS, "hijoDe");
+        Property padreDe = rdfsExample.createProperty(NS, "padreDe");
+        padreDe.addProperty(OWL.inverseOf, hijoDe);
+
+        rdfsExample.add(hijoDe, RDF.type, RDF.Property);
+        hijoDe.addProperty(RDFS.domain, animalClass);
+        hijoDe.addProperty(RDFS.range, animalClass);
+
+        Property hijoAdoptDe = rdfsExample.createProperty(NS, "hijoAdoptivoDe");
+        rdfsExample.add(hijoAdoptDe, RDFS.subPropertyOf, hijoDe);
+        hijoAdoptDe.addProperty(RDFS.domain, humanoClass);
+        hijoAdoptDe.addProperty(RDFS.range, humanoClass);
 		
 		//añadimos las instancias al modelo y sus relaciones
 		Resource luis = rdfsExample.createResource(NS+"Luis");
@@ -56,18 +50,22 @@ public class G_InferenciaOWL2 {
 		System.out.println("------------------------------");
 		rdfsExample.write(System.out,"TURTLE"); 
 		System.out.println("------------------------------");
-		
-		//creamos un modelo de inferencia OWL2
-		InfModel inf = ModelFactory.createInfModel(PelletReasonerFactory.theInstance().create(), rdfsExample);
-					
-		//borramos elementos del modelo para facilitar la visualizacion de lo que nos interesa
-		Model model2 = borrarRecursosOWL(inf);
-		
-		//mostramos el fragmento deseado del modelo inferido
-		System.out.println("------------------------------");
-		model2.write(System.out,"TURTLE"); 
-		System.out.println("------------------------------");
-	}
+
+        //creamos un modelo de inferencia OWL2
+        InfModel inf = ModelFactory.createInfModel(PelletReasonerFactory.theInstance().create(), rdfsExample);
+
+        //borramos elementos del modelo para facilitar la visualizacion de lo que nos interesa
+        Model model2 = borrarRecursosOWL(inf);
+
+        //mostramos el fragmento deseado del modelo inferido
+        System.out.println("------------------------------");
+        model2.write(System.out, "TURTLE");
+        System.out.println("------------------------------");
+
+		/*
+		Al descomentar esas dos líneas se añaden propiedades extra relacionadas con esa inversa, por ejemplo el que ahora el modelo sabe que Luis es padre de Juan (antes no podía deducirlo).
+		 */
+    }
 	
 	/**
 	 * borramos las clases del modelo rdfs que se añaden automáticamene al hacer la inferencia
